e8631badcd1e13cc7d15290da690f15efe1c4168
Gautam Chandra Saha <devgautam1231@gmail.com>
2021-11-07 22:38:52 +0530

    done with register and login frontend

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div>&copy;2021 devhub</div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/screens/Login.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import st from "../css/login.module.css";
+function Login() {
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  const history = useHistory();
+  return (
+    <div className={st.login}>
+      <div>
+        <div className={`${st.circle} ${st.xxlarge} ${st.shade1}`}></div>
+        <div className={`${st.circle} ${st.xlarge} ${st.shade2}`}></div>
+        <div className={`${st.circle} ${st.large} ${st.shade3}`}></div>
+        <div className={`${st.circle} ${st.medium} ${st.shade4}`}></div>
+        <div className={`${st.circle} ${st.small} ${st.shade5}`}></div>
+      </div>
+
+      <div className={st.form}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            value={username}
+            type="email"
+            onChange={(e) => setusername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="Password">Password</label>
+          <input
+            id="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setpassword(e.target.value)}
+          />
+        </div>
+        <button>Sign in</button>
+        <div>
+          <button
+            onClick={() => {
+              history.push("/register");
+            }}
+          >
+            Create account
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
