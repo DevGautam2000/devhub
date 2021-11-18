84b2040fc139d8e280870a9efef0ef62c0b7f50c
Gautam Chandra Saha <devgautam1231@gmail.com>
2021-11-18 18:36:38 +0530

    added rights

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,9 @@ function Footer() {
   return (
     <footer>
       <div>&copy;2021 devhub</div>
+      <div>
+        <small>Gautam Chandra Saha | Rishabh Prasad | Abhishek Mukhia</small>
+      </div>
     </footer>
   );
 }
